60fa5a38cdfac07d956702efe798a074a98c29ec
Irfan <alpergazel@gmail.com>
2022-05-11 17:03:25 -0400

    second commit

--- src/test/java/myfirstproject/Second.java
+++ src/test/java/myfirstproject/Second.java
@@ -3,6 +3,6 @@ package myfirstproject;
 public class Second {
     public static void main(String[] args) {
         System.out.println("this is the second");
-        //changed
+        //changed it
     }
 }
