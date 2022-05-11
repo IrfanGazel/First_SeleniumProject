d993e46aa7114672c1a02dcd79fd15eaecb45140
Irfan <36797472+IrfanGazel@users.noreply.github.com>
2022-05-11 17:02:48 -0400

    Update Second.java

--- src/test/java/myfirstproject/Second.java
+++ src/test/java/myfirstproject/Second.java
@@ -3,6 +3,6 @@ package myfirstproject;
 public class Second {
     public static void main(String[] args) {
         System.out.println("this is the second");
-        //change
+        //changed
     }
 }
